29df6ce34ebd2f6db2b8fe9c4beec0dde806cb07
Ctrl-Tm <73001228+Ctrl-Tim@users.noreply.github.com>
2022-11-15 12:24:41 +0400

    lab5 plots

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template, Response
 import pandas as pd
 import csv
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 app = Flask('__name__')
 
@@ -8,24 +11,6 @@ df = pd.read_csv("coffee.csv")
 
 #read csv, and split on "," the line
 csv_file = csv.reader(open('coffee.csv', "r"), delimiter=",")
-
-kaggleArr = ["my", "Истюков Тимофей (12 вариант) — Цены на кофе",
-             "https://www.kaggle.com/datasets/nancyalaswad90/yamana-gold-inc-stock-price", "Козлов Алексей (13 вариант) — Цены на акции",
-             "https://www.kaggle.com/datasets/nancyalaswad90/diamonds-prices", "Долгов Кирилл (11 вариант) — Цены на бриллианты",
-             "https://www.kaggle.com/datasets/imakash3011/customer-personality-analysis", "Романова Аделина (21 вариант) — Данные о клиентах",
-             "https://www.kaggle.com/datasets/ruchi798/data-science-job-salaries", "Сергеев Евгений (24 вариант) — НЛО",
-             "https://www.kaggle.com/datasets/surajjha101/stores-area-and-sales-data", "Дегтярёв Михаил (9 вариант) — Магазины",
-             "https://www.kaggle.com/datasets/rashikrahmanpritom/heart-attack-analysis-prediction-dataset", "Фасхутдинов Идрис (2 вариант) — Сердечные приступы"
-             ]
-
-ArrKeyWord = ["coffee", "open", "close",
-              "stock", "gold", "yamaha",
-              "diamond", "carat", "clarity",
-              "income", "education", "kidhome",
-              "shape", "duration", "stats",
-              "supermarket", "store", "sales",
-              "age", "sex", "cp",
-              ]
 
 #начальная страница
 @app.route('/')
@@ -37,7 +22,7 @@ def home():
 #справочник
 @app.route('/linear_regression', methods=['GET'])
 def linear_regression():
-    lr_df = df['Close'].reset_index()   # цена закрытия
+    lr_df = df['Close'].reset_index()
     lr_df.rename(columns={'Close': 'Y'}, inplace=True)
     lr_df['X'] = df['Volume']  # объём
     lr_df['XY'] = lr_df['X'] * lr_df['Y']
@@ -59,7 +44,7 @@ def linear_regression():
 #справочник
 @app.route('/more', methods=['GET'])
 def more():
-    lr_df = df['Close'].reset_index()   # цена закрытия
+    lr_df = df['Close'].reset_index()
     lr_df.rename(columns={'Close': 'Y'}, inplace=True)
     lr_df['X'] = df['Volume']  # объём
     lr_df['XY'] = lr_df['X'] * lr_df['Y']
@@ -74,8 +59,42 @@ def more():
     return render_template("more.html", x="Объём", y="Цена закрытия", b0=B0, b1=B1) + lr_df.to_html()
 
 
+@app.route('/lr_plot', methods=['GET'])
+def lr_plot():
+    lr_df = df['Close'].reset_index()
+    lr_df.rename(columns={'Close': 'Y'}, inplace=True)
+    lr_df['X'] = df['Volume']  # объём
+    lr_df['XY'] = lr_df['X'] * lr_df['Y']
+    lr_df['XX'] = lr_df['X'] * lr_df['X']
+    sumY = lr_df['Y'].sum()
+    sumX = lr_df['X'].sum()
+    sumXY = lr_df['XY'].sum()
+    sumXX = lr_df['XX'].sum()
+    N = lr_df['index'].count()
+    B1 = (sumXY - (sumY * sumX) / N) / (sumXX - sumX * sumX / N)
+    B0 = (sumY - B1 * sumX) / N
 
+    plt.scatter(df['Volume'].iloc[:round(N*0.99)], df['Close'].iloc[:round(N*0.99)], color='g')
+    df['TestClose'] = B1 * df['Volume'] + B0
+    plt.title('99% данных')
+    plt.plot(df['Volume'].iloc[:round(N*0.99)], df['TestClose'].iloc[:round(N*0.99)], color='k')
+    # plt.savefig('./plots/99proc.png')
+    tmpfile = BytesIO() #создание временного файла
+    plt.savefig(tmpfile, format='png')
+    plt.clf()
+    encoded1 = base64.b64encode(tmpfile.getvalue()).decode('utf-8') #кодирование
 
+    plt.scatter(df['Volume'].iloc[round(N*0.99):], df['Close'].iloc[round(N*0.99):], color='g')
+    df['TestClose'] = B1 * df['Volume'] + B0
+    plt.title('оставшийся 1% данных')
+    plt.plot(df['Volume'].iloc[round(N*0.99):], df['TestClose'].iloc[round(N*0.99):], color='k')
+    #plt.savefig('./plots/1proc.png')
+    tmpfile = BytesIO()  # создание временного файла
+    plt.savefig(tmpfile, format='png')
+    plt.clf()
+    encoded2 = base64.b64encode(tmpfile.getvalue()).decode('utf-8')  # кодирование
+
+    return render_template("lr_plot.html", encoded1=encoded1, encoded2=encoded2)
 
 
 # запуск HTTP-сервера
